1b84fda46116cdfd35be4bb780582a55ecfd2b25
akshaykumar-07 <126078041+akshaykumar-07@users.noreply.github.com>
2024-09-29 10:36:17 +0530

    Create 1094.cpp

---
+++ Introductory Problems/1094.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define it(i,n) for (int i=0; i<n; i++)
+#define its(i,j,n) for (ll i=j; i<n; i++)
+#define iarr(a)	    for (auto &x: a) {cin>>x;}
+#define oarr(a)	    for (auto &x: a) {cout<<x<<" ";} cout<<endl;
+#define vvl      vector<ll> 
+#define all(a)  a.begin(), a.end()
+#define pb      push_back
+ 
+#define pi      acos(-1)
+#define root(n) pow(n,0.5)
+const ll M = 1e9+7 ;
+/* ______________________________________________________________________________ */
+ 
+void ak();
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+ 
+    int t = 1;
+    // cin>>t;
+
+    while(t--){
+        ak();
+        cout<<endl;
+    } 
+ 
+}
+
+void ak(){
+    ll n;
+    cin>>n ;
+
+    vvl a(n) ;
+    iarr(a) ;
+
+    ll ans = 0 ;
+
+    it (i, n-1) {
+        if (a[i+1] < a[i]) {
+            ans += a[i] - a[i+1] ;
+            a[i+1] = a[i] ;
+        }
+    }
+
+    cout<<ans ;
+    
+}
