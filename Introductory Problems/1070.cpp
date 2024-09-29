e20ff6295108d4d1ffa5d35edb5c540e64a77606
akshaykumar-07 <126078041+akshaykumar-07@users.noreply.github.com>
2024-09-29 10:40:31 +0530

    Create 1070.cpp

---
+++ Introductory Problems/1070.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long int
+#define it(i,n) for (int i=0; i<n; i++)
+#define its(i,j,n) for (ll i=j; i<n; i++)
+#define iarr(a)	    for (auto &x: a) {cin>>x;}
+#define oarr(a)	    for (auto &x: a) {cout<<x<<" ";} cout<<endl;
+#define vvl      vector<ll> 
+#define all(a)  a.begin(), a.end()
+#define pb      push_back
+ 
+#define pi      acos(-1)
+#define root(n) pow(n,0.5)
+const ll M = 1e9+7 ;
+/* ______________________________________________________________________________ */
+ 
+void ak();
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+ 
+    int t = 1;
+    // cin>>t;
+
+    while(t--){
+        ak();
+        cout<<endl;
+    } 
+ 
+}
+
+void ak(){
+    ll n;
+    cin>>n ;
+
+    vvl a ;
+
+    if (n == 1) {
+        cout<<1 ;
+        return ;
+    }
+
+    if (n <= 3) {
+        cout<<"NO SOLUTION" ;
+        return ;
+    }
+
+    for (int i=2; i<=n; i+=2) {
+        a.pb(i) ;
+    }
+
+    for (int i=1; i<=n; i+=2) {
+        a.pb(i) ;
+    }
+
+    oarr(a) ;
+}
